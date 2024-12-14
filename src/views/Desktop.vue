78dfe9bf467265067b0c2c491bd4b66c22bf6502
anvilm <211anvil112@gmail.com>
2024-12-14 18:24:04 +0300

    fix: text

--- src/views/Desktop.vue
+++ src/views/Desktop.vue
@@ -6,8 +6,8 @@ import ModalComponent from "@/components/ModalComponent.vue";
     <div>
         <ModalComponent ref="accessDeny" type="Middle" class="[&>*]:mx-4" :closable="false">
             <div class="flex flex-col w-full">
-                <div class="mb-4 flex justify-center items-center">
-                    <div class="font-semibold text-lg text-text-dark">Приложение доступно только на мобильных устройствах.</div>
+                <div class="flex justify-center items-center">
+                    <div class="font-semibold text-lg text-text-dark text-center">Приложение доступно только на мобильных устройствах.</div>
                 </div>
             </div>
         </ModalComponent>
